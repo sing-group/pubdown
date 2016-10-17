8e3b32dbd39494635d66b8bc2b9dfb7031e5f826
Jesús Álvarez Casanova <jacasanova@esei.uvigo.es>
2016-10-17 09:22:12 +0200

    Updates the sql test file
    Updates the sql test file to include new querys and users.

--- additional-material/pubdown.sql
+++ additional-material/pubdown.sql
@@ -91,6 +91,37 @@ LOCK TABLES `Registration` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `Repository`
+--
+
+DROP TABLE IF EXISTS `Repository`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Repository` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lastUpdate` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `nextUpdate` varchar(255) DEFAULT NULL,
+  `numberOfPapers` int(11) DEFAULT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `userId` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKkct57u0hqa9hqm13ape777xfr` (`userId`),
+  CONSTRAINT `FKkct57u0hqa9hqm13ape777xfr` FOREIGN KEY (`userId`) REFERENCES `User` (`login`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Repository`
+--
+
+LOCK TABLES `Repository` WRITE;
+/*!40000 ALTER TABLE `Repository` DISABLE KEYS */;
+INSERT INTO `Repository` VALUES (1,'11-10-2016 11:41','repo1','',20,'repo1/example','user'),(2,'10-10-2016 11:27','repo2','',4,'repo2','user'),(3,'11-10-2016 11:37','repo3','',6,'repo3','user'),(4,'','user2repo','',0,'user2/repo','user2'),(5,'13-10-2016 10:19','repo4','',4,'repo4','user');
+/*!40000 ALTER TABLE `Repository` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `RepositoryQuery`
 --
 
@@ -100,7 +131,7 @@ DROP TABLE IF EXISTS `RepositoryQuery`;
 CREATE TABLE `RepositoryQuery` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `abstractPaper` bit(1) NOT NULL,
-  `directory` varchar(255) DEFAULT NULL,
+  `checked` bit(1) NOT NULL,
   `fulltextPaper` bit(1) NOT NULL,
   `groupBy` bit(1) NOT NULL,
   `keepPdf` bit(1) NOT NULL,
@@ -109,20 +140,17 @@ CREATE TABLE `RepositoryQuery` (
   `pubmed` bit(1) NOT NULL,
   `pubmedDownloadTo` int(11) NOT NULL,
   `query` varchar(255) DEFAULT NULL,
-  `repository` varchar(255) DEFAULT NULL,
+  `running` bit(1) NOT NULL,
   `scopus` bit(1) NOT NULL,
   `scopusDownloadTo` int(11) NOT NULL,
-  `userId` varchar(255) DEFAULT NULL,
-  `daily` bit(1) NOT NULL,
+  `repositoryId` int(11) DEFAULT NULL,
   `taskId` int(11) DEFAULT NULL,
-  `checked` bit(1) NOT NULL,
-  `running` bit(1) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK7d22y06c6y9dlxi01al8de97g` (`userId`),
-  KEY `FKbkd5cttod6bfi7u1djhe90er2` (`taskId`),
-  CONSTRAINT `FK7d22y06c6y9dlxi01al8de97g` FOREIGN KEY (`userId`) REFERENCES `User` (`login`),
-  CONSTRAINT `FKbkd5cttod6bfi7u1djhe90er2` FOREIGN KEY (`taskId`) REFERENCES `Task` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+  KEY `FKem757sy1gq34ogsargs6mhyh0` (`repositoryId`),
+  KEY `FKbtwsd0my7t1mwk6kjtpfw6167` (`taskId`),
+  CONSTRAINT `FKbtwsd0my7t1mwk6kjtpfw6167` FOREIGN KEY (`taskId`) REFERENCES `RepositoryQueryTask` (`id`),
+  CONSTRAINT `FKem757sy1gq34ogsargs6mhyh0` FOREIGN KEY (`repositoryId`) REFERENCES `Repository` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -131,66 +159,41 @@ CREATE TABLE `RepositoryQuery` (
 
 LOCK TABLES `RepositoryQuery` WRITE;
 /*!40000 ALTER TABLE `RepositoryQuery` DISABLE KEYS */;
-INSERT INTO `RepositoryQuery` VALUES (11,'','user/rep1/','','\0','\0','test1','\0','',2147483647,'vigo and ourense','rep1','',2147483647,'user','',1,'\0','\0'),(12,'','user/rep1/','','\0','\0','test2','\0','',3,'cancer and vigo','rep1','\0',3,'user','\0',2,'\0','\0'),(19,'\0','user/rep1/','','\0','\0','test3','\0','\0',2147483647,'vigo and ourense','rep1','\0',3,'user','',8,'\0','\0'),(22,'\0','user/rep2/','','\0','\0','test5','\0','',2147483647,'vigo and ourense','rep2','\0',2147483647,'user','\0',11,'\0','\0'),(23,'\0','user/rep2/','','\0','\0','test6','','',2147483647,'vigo and cancer','rep2','\0',2147483647,'user','\0',12,'\0','\0'),(24,'','user/rep1/','\0','\0','\0','test4','\0','',3,'vigo and ourense','rep1','\0',2147483647,'user','',13,'\0','\0');
+INSERT INTO `RepositoryQuery` VALUES (19,'\0','','','\0','\0','REPO2_toExecute','\0','',3,'vigo and ourense','\0','',3,2,19),(43,'','','\0','\0','\0','test1_REPO3','\0','',3,'cancer and colon','\0','',3,3,48),(46,'\0','','','\0','\0','test2','\0','',3,'cancer and colon','\0','',3,1,51),(48,'\0','','','\0','\0','REPO1_toExecute','\0','',3,'vigo and ourense','\0','',3,1,53),(49,'','','\0','\0','\0','test2','\0','',3,'vigo and ourense','\0','\0',3,2,54),(50,'\0','','','\0','\0','test3','\0','',3,'cancer','\0','',3,1,55),(51,'\0','','','\0','\0','test4','\0','\0',3,'vigo','\0','',3,1,56),(52,'\0','','','\0','\0','test5','\0','\0',3,'vigo and ourense','\0','',3,1,57),(53,'','','\0','\0','\0','test6','\0','\0',3,'vigo and ourense','\0','',3,1,58),(54,'\0','','','\0','\0','user2query','\0','',3,'vigo and ourense','\0','\0',3,4,59),(55,'\0','','','\0','\0','repo4query','\0','',3,'vigo and ourense','\0','',3,5,60);
 /*!40000 ALTER TABLE `RepositoryQuery` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `RobotExecution`
+-- Table structure for table `RepositoryQueryTask`
 --
 
-DROP TABLE IF EXISTS `RobotExecution`;
+DROP TABLE IF EXISTS `RepositoryQueryTask`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `RobotExecution` (
+CREATE TABLE `RepositoryQueryTask` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `result` longtext,
-  `robotName` varchar(255) NOT NULL,
-  `status` varchar(255) NOT NULL,
-  `userLogin` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `RobotExecution`
---
-
-LOCK TABLES `RobotExecution` WRITE;
-/*!40000 ALTER TABLE `RobotExecution` DISABLE KEYS */;
-/*!40000 ALTER TABLE `RobotExecution` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Task`
---
-
-DROP TABLE IF EXISTS `Task`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Task` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `daily` bit(1) NOT NULL,
+  `friday` bit(1) NOT NULL,
   `hour` int(11) NOT NULL,
   `minutes` int(11) NOT NULL,
   `monday` bit(1) NOT NULL,
-  `tuesday` bit(1) NOT NULL,
-  `wednesday` bit(1) NOT NULL,
-  `thursday` bit(1) NOT NULL,
-  `friday` bit(1) NOT NULL,
   `saturday` bit(1) NOT NULL,
   `sunday` bit(1) NOT NULL,
+  `thursday` bit(1) NOT NULL,
+  `tuesday` bit(1) NOT NULL,
+  `wednesday` bit(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Task`
+-- Dumping data for table `RepositoryQueryTask`
 --
 
-LOCK TABLES `Task` WRITE;
-/*!40000 ALTER TABLE `Task` DISABLE KEYS */;
-INSERT INTO `Task` VALUES (1,13,23,'\0','\0','','\0','\0','\0','\0'),(2,12,13,'\0','\0','','\0','\0','\0','\0'),(8,12,43,'','','','\0','\0','\0','\0'),(10,0,0,'\0','\0','\0','\0','\0','\0','\0'),(11,21,55,'\0','','','\0','\0','\0',''),(12,1,1,'','','','','','',''),(13,12,0,'\0','\0','\0','\0','\0','\0','\0');
-/*!40000 ALTER TABLE `Task` ENABLE KEYS */;
+LOCK TABLES `RepositoryQueryTask` WRITE;
+/*!40000 ALTER TABLE `RepositoryQueryTask` DISABLE KEYS */;
+INSERT INTO `RepositoryQueryTask` VALUES (19,'','\0',11,26,'\0','\0','\0','\0','\0','\0'),(48,'','\0',11,36,'\0','\0','\0','\0','\0','\0'),(51,'','\0',10,8,'\0','\0','\0','\0','\0','\0'),(53,'','\0',11,26,'\0','\0','\0','\0','\0','\0'),(54,'','\0',0,0,'\0','\0','\0','\0','\0','\0'),(55,'','\0',11,41,'\0','\0','\0','\0','\0','\0'),(56,'','\0',0,0,'\0','\0','\0','\0','\0','\0'),(57,'','\0',0,0,'\0','\0','\0','\0','\0','\0'),(58,'','\0',0,0,'\0','\0','\0','\0','\0','\0'),(59,'','\0',0,0,'\0','\0','\0','\0','\0','\0'),(60,'','\0',10,19,'\0','\0','\0','\0','\0','\0');
+/*!40000 ALTER TABLE `RepositoryQueryTask` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -217,7 +220,7 @@ CREATE TABLE `User` (
 
 LOCK TABLES `User` WRITE;
 /*!40000 ALTER TABLE `User` DISABLE KEYS */;
-INSERT INTO `User` VALUES ('user','a1549163d9b16421237ec29c9bbbdf29','user@user.com','\0','$2a$10$R2IC5qcKUFfi0.95cWWL2emXgztldae/i3ExZGDAE2SazmBSTkVEO','USER'),('user2','a1549163d9b16421237ec29c9bbbdf29','user2@user2.com','\0','$2a$10$U7nJQjUJqmfnPhef48/cp.kpEUDKPHmNzbPF6Xegw0Bj1J96EKPXK','USER'),('user3','modifiedUser3','user3@user3.com','\0','$2a$10$6.dfIcragr2iIfMFtJj45ecdi4cDdeCsZr6VINxYqDKE7rlfuwbRy','USER'),('user4','lalalalllaal','user4@user4.com','\0','$2a$10$UZppivaodZp1ws7Q61zRt.FPJjQN.qwILWy09ijALGSRPY4H/6QbK','USER');
+INSERT INTO `User` VALUES ('admin',NULL,'admin@admin.com','\0','$2a$10$zGS0bmgO5l.39ZjtI6765uNVUacuyyw4YNCKxkPoN4rvEPyNGgi6u','ADMIN'),('user','a1549163d9b16421237ec29c9bbbdf29','user@user.com','\0','$2a$10$R2IC5qcKUFfi0.95cWWL2emXgztldae/i3ExZGDAE2SazmBSTkVEO','USER'),('user2','','user2@user2.com','\0','$2a$10$U7nJQjUJqmfnPhef48/cp.kpEUDKPHmNzbPF6Xegw0Bj1J96EKPXK','USER'),('user3','modifiedUser3','user3@user3.com','\0','$2a$10$6.dfIcragr2iIfMFtJj45ecdi4cDdeCsZr6VINxYqDKE7rlfuwbRy','USER'),('user4','lalalalllaal','user4@user4.com','\0','$2a$10$UZppivaodZp1ws7Q61zRt.FPJjQN.qwILWy09ijALGSRPY4H/6QbK','USER');
 /*!40000 ALTER TABLE `User` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -230,4 +233,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-09-27 10:31:20
+-- Dump completed on 2016-10-17  9:21:20
